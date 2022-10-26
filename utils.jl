fc15932a5331088648add87f061e1f393bf3c50b
Jan Kubal <kubaljan@gmail.com>
2022-10-26 19:41:34 +0200

    Starting Part B - data preparation

---
+++ utils.jl
@@ -0,0 +1,40 @@
+""" Create HAR regressors """
+function HAR_regressors(data0)
+
+    RVd = data0[22:end-1] # days
+    RVw = zeros(length(RVd)) # weeks
+    for i in 22:(length(data0)-1)
+       temp = 0
+       for h in 0:4
+           temp = temp + data0[i-h]
+       end
+       RVw[i-21]= temp / 5
+    end
+    
+    RVm = zeros(length(RVd)) # months
+    for i in 22:(length(data0)-1) 
+       temp = 0
+       for h in 0:21
+           temp = temp + data0[i-h]
+       end
+       RVm[i-21] = temp / 22;
+    end
+
+    return [RVd RVw RVm]
+end
+
+"""Train Test split"""
+#very basic implemenattion of train test splitting
+function train_test_split(predictors, target, test_size)
+    break_point = floor(Int, size(predictors)[1]*(1-test_size))
+    X_train = predictors[1:break_point,:] |> permutedims .|> Float32
+    X_test =  predictors[break_point+1:end,:] |> permutedims .|> Float32
+
+    y_train = target[1:break_point,:] |> permutedims .|> Float32
+    y_test = target[break_point+1:end,:] |> permutedims .|> Float32
+    
+    return X_train, X_test, y_train, y_test
+    
+end
+
+println("[> Loaded $(@__FILE__)")
